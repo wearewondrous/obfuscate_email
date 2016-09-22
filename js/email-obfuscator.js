4fd8e5d3fec8a758bf54e1437b7af57d3da23736
pheadeaux <pheadeaux@gmail.com>
2016-09-22 13:58:41 +0200

    use drupal behaviors, add configs

--- js/email-obfuscator.js
+++ js/email-obfuscator.js
@@ -1,35 +1,41 @@
-(function () {
+(function ($, Drupal) {
   'use strict';
 
-  var elements = document.querySelectorAll('a[data-mail-to]');
+  function init(context) {
+    var elements = context.querySelectorAll('a[data-mail-to]');
 
-  if (!elements) {
-    return;
+    if (!elements) {
+      return;
+    }
+
+    // + Jonas Raoni Soares Silva
+    // @ http://jsfromhell.com/string/rot13 [rev. #1]
+
+    String.prototype.rot13 = function () {
+      return this.replace(/[a-zA-Z]/g, function (c) {
+        return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+      });
+    };
+
+    NodeList.prototype.forEach = Array.prototype.forEach;
+
+    elements.forEach(function (element) {
+      var href = element.getAttribute('data-mail-to').rot13();
+      var replaceInner = !!element.getAttribute('data-replace-inner');
+
+      href = href.replace(/\/dot\//g, '.');
+      href = href.replace(/\/at\//g, '@');
+
+      element.setAttribute('href', 'mailto:' + href);
+      element.removeAttribute('data-mail-to');
+
+      if (replaceInner) {
+        element.innerHTML = href;
+      }
+    });
   }
 
-  // + Jonas Raoni Soares Silva
-  // @ http://jsfromhell.com/string/rot13 [rev. #1]
-
-  String.prototype.rot13 = function () {
-    return this.replace(/[a-zA-Z]/g, function (c) {
-      return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-    });
+  Drupal.behaviors.stripeColorField = {
+    attach: init
   };
-
-  NodeList.prototype.forEach = Array.prototype.forEach;
-
-  elements.forEach(function(element) {
-    var href = element.getAttribute('data-mail-to').rot13();
-    var replaceInner = !!element.getAttribute('data-replace-inner');
-
-    href = href.replace(/\/dot\//g, '.');
-    href = href.replace(/\/at\//g, '@');
-
-    element.setAttribute('href', 'mailto:' + href);
-    element.removeAttribute('data-mail-to');
-
-    if (replaceInner) {
-      element.innerHTML = href;
-    }
-  });
-})();
+})(jQuery, Drupal);
