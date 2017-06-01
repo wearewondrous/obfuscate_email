aabe2c45ee1b28d988446156b7fc8da3628bce28
Tassilo Groeper <tassilo@wondrous.ch>
2017-06-01 09:15:17 +0200

    monk on Text Filter

--- src/Plugin/Filter/ObfuscateEmail.php
+++ src/Plugin/Filter/ObfuscateEmail.php
@@ -7,12 +7,12 @@ use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
 
 /**
- * Provides a filter to obfuscate mailto anchor tags.
+ * Provide a filter to obfuscate mailto anchor tags and optionally replace inner text.
  *
  * @Filter(
  *   id = "obfuscate_email",
  *   title = @Translation("Obfuscate Email"),
- *   description = @Translation("Transforms <code>mailto</code> anchors into obfuscated markup"),
+ *   description = @Translation("Transform <code>mailto</code> anchors into obfuscated markup."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
  * )
  */
@@ -24,25 +24,31 @@ class ObfuscateEmail extends FilterBase {
   public function process($text, $langcode) {
     $result = new FilterProcessResult($text);
 
-    if (stristr($text, 'mailto') !== FALSE) {
-      $dom = Html::load($text);
-      $xpath = new \DOMXPath($dom);
-      foreach ($xpath->query('//a[starts-with(@href, "mailto:")]') as $node) {
-        // Read the href attribute value, then delete it.
-        $href = str_replace('mailto:', '', $node->getAttribute('href'));
-        $node->setAttribute('href', '#');
-
-        // Convert to rot13
-        $mail_string = str_rot13(str_replace(['.', '@'], ['/dot/', '/at/'], $href));
-        $node->setAttribute('data-mail-to', $mail_string);
-
-        // Replace an occurrence of the address in the anchor text
-        $node->nodeValue = str_replace($href, '@email', $node->nodeValue);
-        $node->setAttribute('data-replace-inner', '@email');
-      }
-
-      $result->setProcessedText(Html::serialize($dom));
+    if (stristr($text, 'mailto') === FALSE) {
+      return $result;
     }
+
+    $dom = Html::load($text);
+    $xpath = new \DOMXPath($dom);
+
+    /** @var \DOMElement $domElement */
+    foreach ($xpath->query('//a[starts-with(@href, "mailto:")]') as $domElement) {
+      // Read the href attribute value and delete it.
+      $href = str_replace('mailto:', '', $domElement->getAttribute('href'));
+      $domElement->setAttribute('href', '#');
+
+      // Convert to rot13
+      $mail_string = str_rot13(str_replace(['.', '@'], ['/dot/', '/at/'], $href));
+      $domElement->setAttribute('data-mail-to', $mail_string);
+
+      // Replace occurrence of the address in the anchor text.
+      if (strpos($domElement->nodeValue, $href) !== FALSE) {
+        $domElement->nodeValue = str_replace($href, '@email', $domElement->nodeValue);
+        $domElement->setAttribute('data-replace-inner', '@email');
+      }
+    }
+
+    $result->setProcessedText(Html::serialize($dom));
 
     return $result;
   }
